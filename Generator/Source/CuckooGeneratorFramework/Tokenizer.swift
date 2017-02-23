2abf39ab3e5d2bf504d90516b0a87ebcc2eaa08e
Valerij Gerasimenko <mrgerych@s-176-104-14-198.under.net.ua>
2017-02-24 00:10:02 +0200

    fix private(set) variables

--- Generator/Source/CuckooGeneratorFramework/Tokenizer.swift
+++ Generator/Source/CuckooGeneratorFramework/Tokenizer.swift
@@ -100,7 +100,13 @@ public struct Tokenizer {
             return ExtensionDeclaration(range: range!)
             
         case Kinds.InstanceVariable.rawValue:
-            let setterAccessibility = (dictionary[Key.SetterAccessibility.rawValue] as? String).flatMap(Accessibility.init)
+            var containsGetterInBody = false
+            if let bodyRange = bodyRange {
+                let body = source[bodyRange.startIndex..<bodyRange.endIndex].trimmed
+                let containsPropertyObservers = body.contains("didSet") || body.contains("didGet")
+                containsGetterInBody = !containsPropertyObservers
+            }
+            let setterAccessibility = containsGetterInBody ? Accessibility.FilePrivate : (dictionary[Key.SetterAccessibility.rawValue] as? String).flatMap(Accessibility.init)
             
             if String(describing: source.utf8.dropFirst(range!.startIndex)).takeUntil(occurence: name)?.trimmed.hasPrefix("let") == true {
                 return nil
